41d6d12fd5fb15adb94a8ce952aac09721d49fe9
Nikhil Arora <nikhil290192@gmail.com>
2021-05-16 13:14:07 +0530

    Progress

---
+++ twitterBot/twitter.js
@@ -0,0 +1,24 @@
+const { TwitterClient } = require('twitter-api-client');
+
+const tweet = async (status) => {
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY,
+    apiSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_API_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  await twitterClient.tweets.statusesUpdate({ status });
+};
+
+(async () => {
+  try {
+    const myTweet =
+      `#Checkout this #Javascript #basic #projects in #JS #HTML #CSS #Repo on @github https://bit.ly/3eRENGo 
+        Follow: @NiKs290192 on Github and Twitter for more updates :).` ;
+
+    await tweet(myTweet);
+  } catch (err) {
+    console.error(err);
+  }
+})();
